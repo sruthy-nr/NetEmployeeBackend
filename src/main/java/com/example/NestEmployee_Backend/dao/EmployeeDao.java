5d43e34194f74546141c1ae604e74e0403e69cf0
sruthy-nr <sruthyn.@nestgroup.net>
2022-12-12 01:14:14 +0530

    emplogin profile viewemployee

--- src/main/java/com/example/NestEmployee_Backend/dao/EmployeeDao.java
+++ src/main/java/com/example/NestEmployee_Backend/dao/EmployeeDao.java
@@ -2,7 +2,17 @@ package com.example.NestEmployee_Backend.dao;
 
 
 import com.example.NestEmployee_Backend.model.EmployeeModel;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface EmployeeDao extends CrudRepository<EmployeeModel,Integer> {
+
+    @Query(value = "SELECT `id`, `designation`, `email`, `empcode`, `empname`, `password`, `phone`, `salary` FROM `employee` WHERE `email`=:email AND `password`=:password",nativeQuery = true)
+    public List<EmployeeModel> empLogin(@Param("email") String email, @Param("password") String password);
+
+    @Query(value = "SELECT `id`, `designation`, `email`, `empcode`, `empname`, `password`, `phone`, `salary` FROM `employee` WHERE `id`=:id",nativeQuery = true)
+    public  List<EmployeeModel> profileView(@Param("id") Integer id);
 }
