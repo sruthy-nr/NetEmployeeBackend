4a43b26f2e3f7d7910e892fc52be71269581c20d
sruthy-nr <sruthyn.@nestgroup.net>
2022-12-12 00:10:47 +0530

    master

---
+++ src/main/java/com/example/NestEmployee_Backend/NestEmployeeBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.NestEmployee_Backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NestEmployeeBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NestEmployeeBackendApplication.class, args);
+	}
+
+}
